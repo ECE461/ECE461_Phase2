d0fee722b1bf6669703fbcc77a6924c9ff9ee7ca
AidanMDB <121575554+AidanMDB@users.noreply.github.com>
2024-09-22 20:07:39 -0400

    fixed ramp up tests

--- Metric Tests/Ramp Up Tests/ramp-up.tests.ts
+++ Metric Tests/Ramp Up Tests/ramp-up.tests.ts
@@ -6,14 +6,14 @@ describe('Ramp Up Tests', () => {
 
     test('Get Repo Stats', async () => {
         const repo = new rampUp('cloudinary', 'cloudinary_npm');
-        const stats = await repo.getRepoStats();
+        const stats = await repo.getRampUpScore();
         expect(stats).toBe(0.985);
 
     });
 
     test('Get different repo stats', async () => {
         const repo = new rampUp('lodash', 'lodash');
-        const fileCount = await repo.getRepoStats();
+        const fileCount = await repo.getRampUpScore();
         expect(fileCount).toBe(.992);
 
     });
@@ -21,7 +21,7 @@ describe('Ramp Up Tests', () => {
     test('Get empty repo', async () => {
         try {
             const repo = new rampUp('', '');
-            await repo.getRepoStats();
+            await repo.getRampUpScore();
         } catch (error) {
             expect(error).toBeInstanceOf(AxiosError);
         }
